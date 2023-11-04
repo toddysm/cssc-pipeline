95f4aa873bf1e957c789761394c322c2c1aa78a3
Toddy Mladenov <toddysm@gmail.com>
2023-11-04 16:16:25 -0700

    Added info messages
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- utils/sign-oci-layout.sh
+++ utils/sign-oci-layout.sh
@@ -14,30 +14,59 @@ echo "ARCHIVE_NAME: $ARCHIVE_NAME"
 notation version
 
 # Generate a test key for the demo
+echo "Generating a test key for the demo..."
 notation cert generate-test --default "wabbit-networks.io"
 notation key ls
+
+echo
+echo
 
 # Create the OCI layout directory
 mkdir -p $OCI_LAYOUT_LOCATION
 
 # Build the image in OCI layout and save locally
+echo "Building the image in OCI layout and saving locally..."
 docker buildx build . \
     -f Dockerfile \
     -o type=oci,dest=${TEMP_LOCATION}/${ARCHIVE_NAME} \
     --metadata-file ${TEMP_LOCATION}/${BUILD_METADATA_FILE}
 
+echo
+echo
+
 # Extract the OCI layout
+echo
 tar -xvf ${TEMP_LOCATION}/${ARCHIVE_NAME} -C ${OCI_LAYOUT_LOCATION}
 
+echo
+echo
+
 # Show the tree of the OCI layout
+echo "Printing the tree of the OCI layout..."
 tree ${TEMP_LOCATION}
 
+echo
+echo
+
 # Get the manifest digest
+echo "Getting the manifest digest..."
 export IMAGE_DIGEST=`cat ${TEMP_LOCATION}/${BUILD_METADATA_FILE} | jq -r '."containerimage.descriptor".digest'`
+
+echo
+echo
+
+echo "Signing image with digest ${IMAGE_DIGEST}..."
 notation sign --oci-layout ${OCI_LAYOUT_LOCATION}@${IMAGE_DIGEST}
 
+echo
+echo
+
 # Show the list of signatures
+echo "Listing the signatures for image with digest ${IMAGE_DIGEST}..."
 notation list --oci-layout ${OCI_LAYOUT_LOCATION}@${IMAGE_DIGEST}
+
+echo
+echo
 
 # Verify the signature
 export CERT_STORE_NAME=wabbit-networks.io
@@ -63,4 +92,5 @@ cat <<EOF > ${TRUST_POLICY_LOCATION}/trustpolicy.json
 }
 EOF
 
+echo "Verifying the signature for image with digest ${IMAGE_DIGEST}..."
 notation verify --oci-layout ${OCI_LAYOUT_LOCATION}@${IMAGE_DIGEST} --scope $REGISTRY_SCOPE
