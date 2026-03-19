6e3668bcdb859d83ab82ee26cdaa07dccbddfc71
Toddy Mladenov <toddysm@gmail.com>
2026-03-19 15:06:21 -0700

    feat(artifact-signing): update demo script with AKS cleanup, macOS trustpolicy, and list deployments
    - Add kubectl delete cleanup for nginx-signed-demo and nginx-unsigned-demo in
      the pre-run cleanup block (idempotent, --ignore-not-found)
    - Add macOS Notation trustpolicy cleanup path
      (~/Library/Application Support/notation/trustpolicy.json)
    - Add kubectl get all --namespace default before deploying the signed image
      to show the cluster is clean before the demo
    - Add pe kubectl delete cleanup steps at end of demo sequence

--- demos/kubecon-eu-2026/artifact-signing/trusted-signing/demo-trusted-signing.sh
+++ demos/kubecon-eu-2026/artifact-signing/trusted-signing/demo-trusted-signing.sh
@@ -10,7 +10,7 @@
 #
 # pass -h to see all options
 #################################
-. ../demo-magic.sh -d
+. ../demo-magic.sh -n
 
 
 ########################
@@ -20,7 +20,7 @@
 #
 # speed at which to simulate typing. bigger num = faster
 #
-TYPE_SPEED=20
+TYPE_SPEED=100
 
 #
 # custom prompt
@@ -34,12 +34,17 @@ DEMO_PROMPT="${GREEN}$ ${COLOR_RESET}"
 # DEMO_CMD_COLOR=$BLACK
 
 # clean up any previous runs
+SIGNING_TRUST_STORE=kubeconDemoSigningRootCerts
+TSA_TRUST_STORE=kubeconDemoTsaRootCerts
 notation plugin uninstall azure-artifactsigning --yes
 notation cert delete --type ca --store $SIGNING_TRUST_STORE --all --yes
 notation cert delete --type tsa --store $TSA_TRUST_STORE --all --yes
 rm -f ~/.config/notation/trustpolicy.json
+rm -f ~/Library/Application\ Support/notation/trustpolicy.json
 rm -f msft-root-certificate-authority-2020.crt msft-tsa-root-certificate-authority-2020.crt
-rm -fr ~/.config/notation
+kubectl delete -f nginx-signed-demo.yaml --ignore-not-found
+kubectl delete -f nginx-unsigned-demo.yaml --ignore-not-found
+
 
 # hide the evidence
 clear
@@ -59,12 +64,16 @@ pe "TS_TSA_URL=http://timestamp.acs.microsoft.com/"
 pe "TS_SIGNING_ROOT_CERT=\"https://www.microsoft.com/pkiops/certs/Microsoft%20Enterprise%20Identity%20Verification%20Root%20Certificate%20Authority%202020.crt\""
 
 echo
+wait
+
 pe "# Set up variables for ACR and image"
 pe "ACR_LOGIN_SERVER=acrtsmpremiumsku.azurecr.io"
 pe "REPOSITORY=python"
-pe "IMAGE=acrtsmpremiumsku.azurecr.io/python:3.13"
+pe "SIGNED_IMAGE=acrtsmpremiumsku.azurecr.io/nginx:1.29-alpine-signed"
 
 echo
+wait
+
 pe "# Set up variables for signature verification"
 pe "TS_TSA_ROOT_CERT=\"http://www.microsoft.com/pkiops/certs/microsoft%20identity%20verification%20root%20certificate%20authority%202020.crt\""
 pe "SIGNING_TRUST_STORE=kubeconDemoSigningRootCerts"
@@ -72,6 +81,8 @@ pe "TSA_TRUST_STORE=kubeconDemoTsaRootCerts"
 pe "TS_CERT_SUBJECT=\"CN=microsoft.onmicrosoft.com, O=microsoft.onmicrosoft.com, OU=Cloud Native Security and Registries, L=Redmond, S=Washington, C=US\""
 
 echo
+wait
+
 pe "# Log in to ACR"
 pe "az login"
 pe "az acr login --name $ACR_LOGIN_SERVER"
@@ -85,6 +96,8 @@ pe "# Check the plugin"
 pe "notation plugin ls"
 
 echo
+wait
+
 pe "# Install the plugin"
 pe "notation plugin install --url https://github.com/Azure/artifact-signing-notation-plugin/releases/download/v1.1.0/notation-azure-artifactsigning_1.1.0_darwin_arm64.tar.gz --sha256sum f9ff085c86474b2371cf3acd70e24f067df2f5c2c3240e101957d99b55d480f0"
 
@@ -93,32 +106,42 @@ pe "# List the plugin"
 pe "notation plugin ls"
 
 echo
+wait
+
 pe "# Download the TSA root certificate"
 pe "curl -o msft-tsa-root-certificate-authority-2020.crt $TS_TSA_ROOT_CERT"
 
 echo
 pe "# Sign the container image"
-pe "notation sign --signature-format cose --timestamp-url $TS_TSA_URL --timestamp-root-cert "msft-tsa-root-certificate-authority-2020.crt" --id $TS_CERT_PROFILE --plugin azure-artifactsigning --plugin-config accountName=$TS_ACCT_NAME --plugin-config baseUrl=$TS_ACCT_URL --plugin-config certProfile=$TS_CERT_PROFILE $IMAGE"
+pe "notation sign --signature-format cose --timestamp-url $TS_TSA_URL --timestamp-root-cert "msft-tsa-root-certificate-authority-2020.crt" --id $TS_CERT_PROFILE --plugin azure-artifactsigning --plugin-config accountName=$TS_ACCT_NAME --plugin-config baseUrl=$TS_ACCT_URL --plugin-config certProfile=$TS_CERT_PROFILE $SIGNED_IMAGE"
 
 echo
+wait
+
 pe "# List the signature"
-pe "notation ls $IMAGE"
+pe "notation ls $SIGNED_IMAGE"
 
 echo
+wait
+
 pe "# Inpsect the signature"
-pe "notation inspect $IMAGE"
+pe "notation inspect $SIGNED_IMAGE"
 
 echo
+wait
+
 pe "# Set up trust store"
 pe "curl -o msft-root-certificate-authority-2020.crt $TS_SIGNING_ROOT_CERT"
 pe "notation cert add --type ca --store $SIGNING_TRUST_STORE msft-root-certificate-authority-2020.crt"
 pe "notation cert add -t tsa -s $TSA_TRUST_STORE msft-tsa-root-certificate-authority-2020.crt"
 
 echo
-pe "# List the trust store"
+pe "# List the trust stores"
 pe "notation cert ls"
 
 echo
+wait
+
 pe "# Set up trust policy"
 pe "notation policy import trustpolicy.json"
 
@@ -127,12 +150,24 @@ pe "# Show trust policy"
 pe "notation policy show"
 
 echo
+wait
+
 pe "# Verify the image"
-pe "notation verify $IMAGE"
+pe "notation verify $SIGNED_IMAGE"
 
-# run command behind
+echo
+wait 
 
-# enters interactive mode and allows newly typed command to be executed
+pe "# Let's deploy a demo application using the signed image"
+pe "kubectl get all --namespace default"
+pe "kubectl apply -f nginx-signed-demo.yaml"
+pe "kubectl get svc nginx-signed-demo -w"
+
+echo
+wait
+
+pe "# Now let's try to deploy an unsigned image and see it get blocked by the policy"
+pe "kubectl apply -f nginx-unsigned-demo.yaml"
 
 # show a prompt so as not to reveal our true nature after
 # the demo has concluded
